16d11470fd9b22dd066a604d1069e3b03d982ba7
toran billups <toranb@gmail.com>
2016-04-03 19:13:13 -0500

    alternateRoute

--- app/users/route.js
+++ app/users/route.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
-import route from 'ember-redux/route';
 import ajax from 'example/utilities/ajax';
 
-var model = (dispatch) => {
-    return ajax('/api/users', 'GET').then(response => dispatch({type: 'DESERIALIZE_USERS', response: response}));
-};
+var UsersRoute = Ember.Route.extend({
+    redux: Ember.inject.service(),
+    model() {
+        var redux = this.get('redux');
+        return ajax('/api/users', 'GET').then(response => redux.dispatch({type: 'DESERIALIZE_USERS', response: response}));
+    }
+});
 
-var UsersRoute = Ember.Route.extend();
-
-export default route({model})(UsersRoute);
+export default UsersRoute;
